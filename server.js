2ed7e980ba97f1c525e718573cd29978da932aad
Mohamed Yasser <mohamedyyy06@gmail.com>
2026-09-05 00:04:50 +0300

    fix

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express'
 const app = express()
-app.use(express.static('public'))
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
 
 const port = process.env.PORT || 3000
 import {Server} from 'socket.io'
